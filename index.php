efb21288b8b2b485049b90b88b200c6cb22514f9
Zachary Torricelli <35783419+zacharytorricelli@users.noreply.github.com>
2019-05-03 09:55:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,354 @@
 <?php include "templates/header.php"; ?>
 
+<style type="text/css">      
+{box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+      
+      
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #1A237E;
+  float: none;
+  text-align:center;
+}
+      
+      
+li {
+  display:inline;
+}
+
+      
+li a, .dropbtn {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+      
+li a:hover, .dropdown:hover .dropbtn {
+  background-color: black;
+}
+
+      
+li.dropdown {
+  display: inline-block;
+}
+
+      
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: gray;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+      
+.dropdown-content a {
+  color: white;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+      
+.dropdown-content a:hover {
+  background-color: black
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+
+}
+
+* {
+  box-sizing: border-box;
+}
+
+form.example input[type=text] {
+  padding: 10px;
+  font-size: 17px;
+  border: 1px solid #01579B;
+  float: left;
+  width: 80%;
+  background: #f1f1f1;
+  
+}
+
+form.example button {
+  float: left;
+  width: 15%;
+  padding: 10px;
+  background: #1A237E;
+  color: white;
+  font-size: 17px;
+  border: 1px solid #01579B;
+  border-left: none;
+  cursor: pointer;
+}
+
+form.example button:hover {
+  background: black;
+}
+
+form.example::after {
+  content: "";
+  clear: both;
+  display: table;
+}   
+   .dropdown-centered a {
+    position: relative;
+    top: 0;
+    left: 0;
+    transform: none;
+}
+
+    .pull-right {
+    float: right;
+}     
+
+
+.footer {
+   position: relative;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 70%;
+   background-color: #01579B;
+   color: white;
+   text-align: center;
+}
+
+a:link {
+  color: white;
+}
+
+.btn {
+  background-color: #01579B;
+  border: none;
+  color: black;
+  padding: 0px 10px;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #01579B;
+  color: white
+}
+
+body {margin:0;}
+
+.icon-bar {
+  width: 80%;
+  background-color: navy;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  float: none;
+  text-align:center;
+  margin: auto;
+  justify-content: center;
+}
+
+.icon-bar a{
+  float: left;
+  width: 20%;
+  text-align: center;
+  padding: 12px 0;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 40px;
+  
+}
+
+.icon-bar a:hover {
+  background-color: #000;
+}
+
+.active {
+  background-color: navy;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 30%;
+  background-color: white;
+}
+
+td, th {
+  border: 5px solid DarkBlue;
+  text-align: center;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: white;
+}
+
+td a:link{
+  color: #37474F;
+}
+</style>
+
+
+
 <html>
   <head>
-   
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
-  
- 
-<h2 style= "font-family:helvecta; color:white;"><i>"The World's Leading Technology Store"</i></h2>
+
+    <ul>
+     <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Products</a>
+    <div class="dropdown-content">
+      <a>TVs</a>
+      <a>Computers & Laptops</a>
+      <a>Tablets</a>
+      <a>Cell Phones</a>
+      <a>Cameras & Camcorders</a>
+      <a>Home Theatre</a>
+      <a>All Products</a>
+       
+      
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Brands</a>
+    <div class="dropdown-content">
+      <a>Apple</a>
+      <a>Samsung</a>
+      <a>Sony</a>
+      <a>Microsoft</a>
+      <a>Intel</a>
+      <a>All Brands</a>
+      
+      
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Deals</a>
+    <div class="dropdown-content">
+      <a>Hottest Deals</a>
+      <a>Deal of the Day</a>
+      <a>Member Offers</a>
+      <a>Clearance & More</a>
+      
+      
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Services</a>
+    <div class="dropdown-content">
+      <a>Goon Squad</a>
+      <a>AppleCare</a>
+      <a>Customer Service</a>
+      <a>Check Repair Status</a>
+      <a>All Services</a>
+    </div>
+  </li>
+</ul>
+
+
+    <br><br>
+    
+    
+    
+    
+      
       
       
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-
+<div class="icon-bar">
+  <a><i class="fa fa-tv"></i></a> 
+  
+    <a><i class="fa fa-laptop"></i></a> 
+  
+  <a><i class="fa fa-tablet"></i></a> 
+  
+  <a><i class="fa fa-gamepad"></i></a>
+  
+  <a><i class="fa fa-headphones"></i></a>
+  
+  <a><i class="fa fa-mobile"></i></a>
+  
+  <a><i class="fa fa-bluetooth"></i></a>
+  
+  <a><i class="fa fa-wifi"></i></a>
+  
+  <a><i class="fa fa-child"></i></a>
+  
+  <a><i class="fa fa-fighter-jet"></i></a>
+  
+  <a><i class="fa fa-camera"></i></a>
+  
+  <a><i class="fa fa-clock-o"></i></a>
+  
+  <a><i class="fa fa-columns"></i></a>
+  
+  <a><i class="fas fa-blender"></i></a>
+  
+  <a><i class="fa fa-dot-circle-o"></i></a>
+  
+  <a><i class="fa fa-print"></i></a>
+  
+  <a><i class="fa fa-car"></i></a>
+  
+  <a><i class="fas fa-store"></i></a>
+  
+  </div>
       
     <br><br>
 
  <center>
 <table>
-
+ 
   <tr>
-    <td><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/M/ME/MMEF2/MMEF2?wid=572&hei=572&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1503962928226"style="height:200px;witdh:170px">
-        <center> Apple Airpods </center>
-
-    <td> <img src="https://images-na.ssl-images-amazon.com/images/I/81VnTSgQgQL._SX569_.jpg"style="height:200px;width:170px">
-      <center> Samsung 9s </center>
+    <td><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/M/ME/MMEF2/MMEF2?wid=572&hei=572&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1503962928226"style="height:200px;witdh:170px"></img>
+    <p>Save $30 on Apple AirPods</p>
+        <a><b>Apple Airpods</b></a>
+        <p>Get 20% off of Apple Airpods</p>
+    </td>
+      
+    <td> <img src="https://images-na.ssl-images-amazon.com/images/I/81VnTSgQgQL._SX569_.jpg"style="height:200px;width:170px"></img>
+      <p> Save $360 on Samsung 9s </p>
+        <a><b>Samsung 9s</b></a>
+        <p>Get 50% off of Samsung 9s</p>
+        </td>
+         
+    <td> <img src="https://www.flexsealproducts.com/wp-content/uploads/2018/05/flex-tape-waterproof-clear-large.jpg"style="height:200px;width:170px"></img>
+      <p>Save $7.53 off of Flex(Duct)
+        Tape</p>
+    <a><b>Flex Tape</b></a>
+      <p>Get 58% off of FlexTape</p>
+        </td>
+         
+  </tr>
+  <tr>
+    <td><img src="https://www.t-mobile.com/content/dam/t-mobile/en-p/cell-phones/apple/apple-iphone-x/space-gray/Apple-iPhoneX-SpaceGray-1-3x.jpg"style="height:200px;width:170px"></img> <p>Save $187.25 off of iPhone X</p> 
+      <a><b>iPhone X</b></a> 
+      <p>Get 25% off of iPhone X</p>
+      </td>
     
-    <td> <img src="https://www.flexsealproducts.com/wp-content/uploads/2018/05/flex-tape-waterproof-clear-large.jpg"style="height:200px;width:170px">
-      <center> Flex(Duct) Tape</center>
+    <td><img src="https://i5.wal.co/asr/49acb5d0-2961-48fc-9502-9390d304a175_1.7476a7937c7f21bbe791079c7fcac10e.jpeg-38566084de909930b13b442f517f83160d7961d3-optim-450x450.jpg?odnBg=FFFFFF"style="height:200px;width:170px"></img> 
+    <p>Save $97.99 off of iPad</p> 
+        <a><b>iPad</b><a> 
+        <p>Get 40% of off iPad</p>
+        </td>
         
-    </tr>
-  <tr>
-    <td><img src="https://www.t-mobile.com/content/dam/t-mobile/en-p/cell-phones/apple/apple-iphone-x/space-gray/Apple-iPhoneX-SpaceGray-1-3x.jpg"style="height:200px;width:200px"> <center> iPhone X</center> 
-        
-    <td><img src="https://i5.wal.co/asr/49acb5d0-2961-48fc-9502-9390d304a175_1.7476a7937c7f21bbe791079c7fcac10e.jpeg-38566084de909930b13b442f517f83160d7961d3-optim-450x450.jpg?odnBg=FFFFFF"style="height:200px;width:170px"> <center> iPad</center> 
-        
-    <td><img src="https://d1whcn1ntmec99.cloudfront.net/images/catalog/products/boxer/boxer-interactive-ai-robot-toy-blue/full1.jpg"style="height:200px;width:170px"> <center> SpinMaster</center> 
+    <td><img src="https://d1whcn1ntmec99.cloudfront.net/images/catalog/products/boxer/boxer-interactive-ai-robot-toy-blue/full1.jpg"style="height:200px;width:170px"></img> 
+    <p>Save $10 off of SpinMaster</p> 
+        <a><b>SpinMaster</b></a> 
+        <p>Get 10% off of SpinMaster</p>
+        </td>
   </tr>
 </table>
 
@@ -46,46 +357,68 @@
 
  <center>
 <table>
+ 
   <tr>
     
     
-    <td> 
-      <img src="https://cdn2.iconfinder.com/data/icons/shopping-online-e-commerce-store/512/delivery_shipping_arrival_free-512.png"style="height:100px;width:200px">   
-      <h3><p>Free Shipping</p></h3>
-     </td>
+    <td>
+      
+      <a>
+      <img src="https://cdn2.iconfinder.com/data/icons/shopping-online-e-commerce-store/512/delivery_shipping_arrival_free-512.png"style="height:100px;width:200px">
+      </a>
+      
+      <p><h3>Free Shipping</center></p></h3></td>
+
+
    <td>
+     
+     
+     <a>
      <img src="https://www.janusid.nl/media/fastcheckin.gif"style="height:100px;width:200px">
-     <h3><p>Fast Store Pickup</p></h3>
-    </td>
+     </a>
+
+     <p><h3>Fast Store Pickup</p></h3></td>
 
 
 <td>
   
+  
+  <a>
   <img src="https://www.senpex.com/upload/page_img/ef2fa3b36132226620c066436705e711.png"style="height:100px;width:200px">
-<h3><p>Same-Day Pickup</p></h3>
+  </a>
+<h3><p>Same-Day Pickup</h3></p>
 
-    </td>
+    </td></tr>
+</table>
 
 
 <br><br>
 
     <div class="footer">
       <br>
-      <h5>Corporate Information | Forums, Blog & More | Careers | Accessibility | Terms and Conditions | Privacy | Interest-Based Ads | Privacy Rights 
+      <h5><a>Corporate Information</a> | 
+          <a>Forums, Blog & More</a> | 
+          <a>Careers</a> | 
+          <a>Accessibility </a> | 
+          <a>Terms and Conditions</a> | 
+          <a>Privacy </a>| 
+          <a>Interest-Based Ads </a>| 
+          <a>California Privacy Rights </a>| 
+          <a>CA Supply Chain Transparency Act</a>
         </h5>
       
-        <h6>Employees must maintain the confidentiality of intellectual property and all other confidential information that belongs to the Sonic Electronics.<br>
-    Intellectual property includes identifiable names, trademarks, logos, images, graphics and other copyrighted materials of Sonic Electronis or any of its subsidiaries.<br>
-  
-    Sonic Electronic's proprietary information and competitive knowledge is confidential. 
-  It is limited for use within Sonic Electronic unless specifically authorized for release and disclosure elsewhere.
-        </h6>
+<h6>Employees must maintain the confidentiality of intellectual property and all other confidential information that belongs to the Sonic Electronics.<br>
+    Intellectual property includes identifiable names, trademarks, logos, images, graphics and other copyrighted materials of Sonic Electronics or any of its subsidiaries.<br>
+  Sonic Electronics' proprietary information and competitive knowledge is confidential. 
+  It is limited for use within Sonic Electronics unless specifically authorized for release and disclosure elsewhere.</h6>
       
     <button class="btn"><i class="fab fa-instagram"></i></button>
 <button class="btn"><i class="fab fa-pinterest"></i></button>
 <button class="btn"><i class="fab fa-facebook"></i></button><button class="btn"><i class="fab fa-twitter-square"></i></button>
     </div>
     
+</body>
+</html>
 
 <h1>Database Controls</h1>
 <ul>
@@ -122,7 +455,7 @@
         <a href="querySeven.php"><strong>Query 7</strong></a>
     </li>
 </ul>
-
+     
 
 
 <?php include "templates/footer.php"; ?>
