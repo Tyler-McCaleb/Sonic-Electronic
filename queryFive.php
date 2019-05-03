185fe7559aa03cd5821e85d6df7d9798036d02e2
Zachary Torricelli <35783419+zacharytorricelli@users.noreply.github.com>
2019-05-03 10:19:04 -0500

    Update queryFive.php

--- queryFive.php
+++ queryFive.php
@@ -16,14 +16,15 @@ try
     $connection = new PDO($dsn, $username, $password, $options);
 
     // create the querery
-    $sql = "SELECT 'desired information'
-    FROM 'table name'
-    WHERE 'conditions'"
+    $sql = "SELECT item_id, product_name, SUM(quantity), region
+            FROM product NATURAL JOIN inventory
+            WHERE quantity = 0 AND region = 'California'
+            GROUP BY item_id
+            ORDER BY item_id DESC";
             
 
     // Prepare the statement
     $statement = $connection->prepare($sql);
-    $statement->bindParam(':location', $location, PDO::PARAM_STR); // bind :location in the query to the variable $location  (Just an example to show binding parameter, may not be needed in actual code)
     $statement->execute();    // execute the statement
 
     // save all the data from the statemet into the result
@@ -38,5 +39,35 @@ catch(PDOException $error)
 
 ?>
 
+<!-- If there are results put them into a table -->
+<?php
+    if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Products that are out of stock in California</h2>
+    <table>
+    <thead>
+    <tr>
+    <th>Results:</th>
+    </tr>
+<   /thead>
+    <tbody>
+        <?php foreach ($result as $row) { ?>
+        <tr>
+        <?php
+            $due = new DateTime(escape($row["due_date"]));
+            $received = new DateTime(escape($row["receive_date"]));
+            ?>
+        <?php if ($received > $due) { ?>
+        <td><?php echo escape($row["item_id"]); ?></td>
+        <td><?php echo escape($row["product_name"]); ?></td>
+        <td><?php echo escape($row["quantity"]); ?></td>
+        <td><?php echo escape($row["region"]); ?></td>
+        <?php } ?>
+        </tr>
+        <?php } ?>
+    </tbody>
+    </table>
+    <?php } else { ?>
+        There are no products out of stock in California.
+<?php } ?>
 
 <?php include "templates/footer.php"; ?>
