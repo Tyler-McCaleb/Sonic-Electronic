77912df0178cffbe9a9fecdb783de743cd79f54b
Zachary Torricelli <35783419+zacharytorricelli@users.noreply.github.com>
2019-05-04 15:46:11 -0500

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang = "en">
     <head>
-         <title> Sonic Electronics </title>
+        
+        <title> Sonic Electronics </title>
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>
@@ -18,12 +19,12 @@
   
   
   <form class="example pull-right" action="/action_page.php" style="margin:auto;max-width:300px">
-  <input type="text" placeholder="Search.." name="search2">
+  <input type="text" placeholder="Search..." name="search2">
   <button type="submit"><i class="fa fa-search"></i></button>
-</form>
+    </form>
   
 
-<img src="https://www.logolynx.com/images/logolynx/56/569a9c6f670baae2a9be83ef3a8a3364.png" style="height: 125px; width: 175px;">
+<img src="https://www.logolynx.com/images/logolynx/56/569a9c6f670baae2a9be83ef3a8a3364.png" style="height: 125px; width: 175px";>
     
     <h1 style= "font-size: 20px";> Sonic Electronics </h1>
     
