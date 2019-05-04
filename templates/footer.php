d97f5f6271c5a0b4a155bee0c9543e73186ca6b7
Zachary Torricelli <35783419+zacharytorricelli@users.noreply.github.com>
2019-05-04 15:47:40 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,48 @@
 <html>
+<head>
+    
+     <link rel = "stylesheet" href = "css/style.css">
+    
+</head>
 <body>
 
-<a style = "color: black" href="index.php";>Home</a>
+    <h1><center>Database Controls</center></h1>
+<ul>
+    <li>
+        <a href="install.php">Create the Database</a>
+    </li>
+    <li>
+        <a href="destroy.php">Destroy the Database</a>
+    </li>
+</ul>
+
+
+        <h2><center>Queries</center></h2>
+<ul>
+    <li>
+        <a href="queryOne.php"><strong>Query 1</strong></a>
+    </li>
+    <li>
+        <a href="queryTwo.php"><strong>Query 2</strong></a>
+    </li> 
+    <li>
+        <a href="queryThree.php"><strong>Query 3</strong></a>
+    </li>
+    <li>
+        <a href="queryFour.php"><strong>Query 4</strong></a>
+    </li>
+    <li>
+        <a href="queryFive.php"><strong>Query 5</strong></a>
+    </li>
+    <li>
+        <a href="querySix.php"><strong>Query 6</strong></a>
+    </li>
+    <li>
+        <a href="querySeven.php"><strong>Query 7</strong></a>
+    </li>
+</ul>
+     
+    <a style = "color: black" href="index.php";><center>Home</center></a>
 
 </body>
 </html>
